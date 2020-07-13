23ad084d7d6a183a958b0de32bcb9e8f136299f6
Kyle Casteline <Kyle.Casteline@domain.local>
2020-07-13 10:50:36 -0700

    initial commit

---
+++ BlockerGen1.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python
+import serial
+import time
+
+## Pump fill configuration: ##
+
+tipvolume = 100 #how much you want to fill the tip here in ul
+
+dispensevolume = 1.5 #how much you want to dispense per pump dispense command here in ul
+
+stepsul = 192 #steps/ul 
+fv = bytes(str(stepsul*tipvolume), 'ascii') #actual steps to fill the tip used in the filling def converted to str for command
+dv = bytes(str(stepsul*dispensevolume), 'ascii') #acutal dispense steps converted to str for command
+
+## Robot configuration ##
+
+xfeed = 10000
+yfeed = 10000
+zfeed = 2000
+
+# This is the position in the back left corner of the nest #
+xoffset = -53.1
+yoffset = -195.35
+zoffset = -20
+
+## Convert robot config to bytes ##
+xoffset = bytes(str(xoffset), 'ascii')
+yoffset = bytes(str(yoffset), 'ascii')
+zoffset = bytes(str(zoffset), 'ascii')
+
+xfeed = bytes(str(xfeed), 'ascii')
+yfeed = bytes(str(yfeed), 'ascii')
+zfeed = bytes(str(zfeed), 'ascii')
+
+## Open serial ports ##
+robot = serial.Serial('/dev/ttyACM0',115200)
+pump = serial.Serial('/dev/ttyUSB0' , 9600)
+print('opening Serial Ports')
+ 
+## Open g-code file (only place one gcode file each gcode folder on the desktop) ##
+#g1file = open('/home/pi/Desktop/gcode1/gcode1.gcode','r')
+g2file = open('/home/pi/Desktop/gcode2/gcode2.gcode','r')
+print('opening gcode files')
+
+## cleanup gcode strings ##
+def removeComment(string):
+    if (string.find(';')==-1):
+        return string
+    else:
+        return string[:string.index(';')]
+
+## this function is used to wait for move to complete ##
+def robotwait():
+    robot.flush()
+    while True:
+        robot_out = str(robot.readline()) #wait for response
+        print(' robot: ' + robot_out.strip())
+        if robot_out[2] == 'Z': #get an Z_move_comp response
+            break
+
+## wait to send next command to not crash the pump contoller ##
+def pumpwait():
+    pump.flush()
+    while True:
+        time.sleep(.5)
+        pump.flushOutput()
+        pump.write(b'/1?\r\n')  #query the pump's position
+        pump_out = str(pump.readline()) #wait for response
+        print(' pump: ' + pump_out.strip())
+        if pump_out[8] == '`': #get a done moving response from the pump
+            time.sleep(1)
+            break
+             
+## for use in motion functions, enter x position and feedrate (default will be used if no value provided) ##
+def xmove(xm, xf):
+    xm = bytes(str(xm), 'ascii')
+    xf = bytes(str(xf), 'ascii')
+    robot.write(b'G1 X' + xm + b' ' + xf + b' ;\r\n')
+    robotwait()
+
+## for use in motion functions, enter y position and feedrate (default will be used if no value provided) ##
+def ymove(ym, yf):
+    ym = bytes(str(ym), 'ascii')
+    yf = bytes(str(yf), 'ascii')
+    robot.write(b'G1 Y' + ym + b' F' + yf + b' ;\r\n')
+    robotwait()
+
+## for use in motion functions, enter x and y positions and feedrate (default will be used if no value provided) ##
+def xymove(xxym, xyym, xf):
+    xxym = bytes(str(xxym), 'ascii')
+    xyym = bytes(str(xyym), 'ascii')
+    xf = bytes(str(xf), 'ascii')
+    robot.write(b'G1 X' + xxym + b' Y' + xyym + b' F' + xf + b' ;\r\n')
+    robotwait()
+    
+## for use in motion functions, enter z position and feedrate (default will be used if no value provided) ##
+def zmove(zm, zf):
+    zm = bytes(str(zm), 'ascii')
+    zf = bytes(str(zf), 'ascii')
+    robot.write(b'G1 Z' + zm + b' F' + zf + b' ;\r\n')
+    robotwait()
+
+## Home/initialize function ##
+def initialize():
+    
+    #wake up pump
+    pump.write(b'\r\n\r\n') #don't know if the pump sleeps
+    time.sleep(1)
+    pump.flushInput()
+    pump.write(b'/1W4R\r\n') #initialize the pump
+    print('initializing the pump')
+    pumpwait()
+
+
+    # Wake up robot
+    print('initializing robot')
+    robot.write(b'\r\n\r\n') # Hit enter a few times to wake the controller
+    time.sleep(1)   # Wait for controller to wake up
+    robot.flushInput()  # Flush startup text in serial input
+    robot.write(b'M203 X' + xfeed + b' Y' + yfeed + b' Z' + zfeed  + b' ;\r\n') #set feedrates
+    zmove(50, 2000)
+    robot.write(b'G28 ;\r\n') #home the XYZ robot
+    robotwait()
+    robot.write(b'G54 ;\r\n') #switch to workspace 1
+    robot.write(b'G92 X' + xoffset + b' Y' + yoffset + b' Z' + zoffset + b' ;\r\n') #Zero to offset position
+    zmove(50, 2000)
+    ymove(40, 3000)
+    
+## dispense paths that will be used in a pattern ##
+def dispensepath1():
+    # Stream g-code
+    g1file = open('/home/pi/Desktop/gcode1/gcode1.gcode','r') #open and read gcode file
+    dispensed = False
+    for line in g1file:
+        l = line
+        l = l.strip() # Strip all EOL characters for streaming
+        if  (l.isspace()==False and len(l)>0) :
+            print('Sending: ' + l)
+            robot.write(bytes(l, 'ascii') + b'\n') # Send g-code block
+            if l[1] == '1':
+                robotwait()
+        if not dispensed:
+            pump.write(b'/1D230R\r\n') #first drop
+            time.sleep(.5)
+            dispensed = True
+
+def dispensepath2():
+    # Stream g-code
+    for line in g2file:
+        l = line
+        l = l.strip() # Strip all EOL characters for streaming
+        if  (l.isspace()==False and len(l)>0) :
+            print('Sending: ' + l)
+            robot.write(bytes(l, 'ascii') + b'\n') # Send g-code block
+            robot_out = str(robot.readline()) # Wait for response with carriage return
+            print(' robot: ' + robot_out.strip())
+
+## Fill the system tubing etc. with fluid ##
+def sysprime():
+    pump.write(b'/1W4A40000OA0IA40000OA0IA40000OA0IM2000A9600OA0IR\r\n') #re-init
+    pumpwait()
+
+## the tip filling process ##
+def fill():
+    print('filling...')
+    zmove(100, 2000)     
+    xymove(30.1, 18.4, 5000) #this is just an example position where the vials will be
+    zmove(15, 1000) #move tips down into vials
+    zmove(-5, 500) #slow move to bottom
+    pump.write(b'/1A' + fv + b'D400R\r\n') #fill tip and dispense 400 steps
+    time.sleep(5)
+    zmove(50, 2000) #move Z up
+    #pumpwait()
+
+## Empyting the tips ##
+def empty():
+    print('emptying...')
+    zmove(100, 1000)        
+    xymove(97, 17.2, 5000) #this is just an example position where the vials will be
+    zmove(15, 1000) #move tips down into vials
+    pump.write(b'/1A0R\r\n') #move the syringes to 0
+    time.sleep(8) #wait just in case
+    zmove(100, 2000) #move Z up
+    #pumpwait()
+
+## Move the tip to the back left corner of the nest to check calibration ##
+def origin():
+    zmove(100, 1000)
+    xymove(0, 0, 1000)
+    zmove(10, 1000)
+    zmove(0, 500)
+    
+##pre-run prime, used to get air pocket out of tips
+def tipprime():
+    zmove(50, 1000)
+    xymove(97, 17.2, 5000)
+    zmove(33, 1000)
+    pump.write(b'/1D800R\r\n')
+    time.sleep(1)
+    ymove(15, 500)
+    zmove(50, 1000)
+
+## run babalu chips ##
+def babalu():
+    print('running babalu tray')
+    zmove(50, 1000) #move Z up just in case
+    tipprime()
+    xymove(59.14, -145.5, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-.5, 500) #move down to the first spot
+    for i in range(3):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 2:
+            xymove(0, 49.53, 2000) #move back to the next spot
+        if i < 2:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    robot_out = str(robot.readline()) # Wait for response with carriage return
+    print(' robot: ' + robot_out.strip())
+    zmove(50, 1000) #move up
+    ymove(40, 3000)
+
+## run falcon ##
+def falcon():
+    print('running babalu tray')
+    zmove(50, 1000) #move Z up just in case
+    tipprime()
+    xymove(75.91, -145.5, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-.5, 500) #move down to the first spot
+    for i in range(3):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 2:
+            xymove(0, 49.53, 2000) #move back to the next spot
+        if i < 2:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    robot_out = str(robot.readline()) # Wait for response with carriage return
+    print(' robot: ' + robot_out.strip())
+    zmove(50, 1000) #move up
+    ymove(40, 3000)
+
+## run cardea carrier##
+def cardea():
+    print('running cardea tray')
+    zmove(50, 1000) #move Z up just in case
+    tipprime()
+    xymove(27.42, -140.6, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-.5, 500) #move down to the first spot
+    for i in range(5):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 4:
+            xymove(0, 28.58, 2000) #move back to the next spot
+        if i < 4:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    xymove(93.95, -140.6, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-1.6, 500) #move down to the first spot
+    for i in range(5):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 4:
+            xymove(0, 28.58, 2000) #move back to the next spot
+        if i < 4:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    robot_out = str(robot.readline()) # Wait for response with carriage return
+    print(' robot: ' + robot_out.strip())
+    zmove(50, 1000) #move up
+    ymove(40, 3000)
+
+## run cardea carrier##
+def gaia():
+    print('running cardea tray')
+    zmove(50, 1000) #move Z up just in case
+    tipprime()
+    xymove(27.42, -140.6, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-.5, 500) #move down to the first spot
+    for i in range(5):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 4:
+            xymove(0, 28.58, 2000) #move back to the next spot
+        if i < 4:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    xymove(93.95, -140.6, 5000) #move to the start of the first spot
+    zmove(5, 2000) #rapid move
+    zmove(-1.6, 500) #move down to the first spot
+    for i in range(5):
+        robot.write(b'G55 ;\r\n') #set position coordinate system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        robot.write(b'G92 X0 Y0 Z0 ;\r\n') #zeros system
+        robot_out = str(robot.readline()) # Wait for response with carriage return
+        print(' robot: ' + robot_out.strip())
+        dispensepath1()
+        time.sleep(.5)
+        zmove(10, 1000) #move up
+        if i < 4:
+            xymove(0, 28.58, 2000) #move back to the next spot
+        if i < 4:
+            zmove(0, 500)
+    robot.write(b'G54 ;\r\n') #back to cord system 1
+    robot_out = str(robot.readline()) # Wait for response with carriage return
+    print(' robot: ' + robot_out.strip())
+    zmove(50, 1000) #move up
+    ymove(40, 3000)
+
+## Terminal loop to use inplace of gui ##
+#while True: 
+#   inputchar = str(input())
+#   if inputchar == 'i': #type 'i' in console to initialize
+#       initialize()
+#   elif inputchar == 'f': #type 'f' in console to fill [note this will probably change depending on the reservior arrangement]
+#        fill()
+#   elif inputchar == 'r1': #type 'r1' to run tray 1
+#       babalu()
+#   elif inputchar == 'r2': #type 'r2'
+#        cardea()
+#   elif inputchar == 'e': #type 'e' to empty the tips
+#        empty()
+
+
+#Close file and serial port [saving in case I decide to use these later for shutdown or something]
+#gfile.close()
+#robot.close()
+#pump.close()
---
+++ BlockerGen1_gui.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import tkinter
+from tkinter import messagebox
+from tkinter import *
+import tkinter.font as font
+from BlockerGen1 import *
+
+## GUI ##
+root = Tk()
+root.title('Automated Spotter Gen1')
+#root.config(cursor='none') 
+#root.geometry('800x480') #windowed mode for dev
+root.attributes('-fullscreen', True) 
+
+buttonFont = font.Font(family='Helvetica', size=18, weight='bold')
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+
+def ExitApplication():
+    MsgBox = messagebox.askquestion('Exit Application','Are you sure you want to exit the application?',icon = 'warning')
+    if MsgBox == 'yes':
+        robot.close()
+        pump.close()
+        root.destroy()
+    else:
+        messagebox.showinfo('Return','You will now return to the application screen.')
+
+initButton = Button(root, text='INITIALIZE', bg='springgreen', command=initialize)
+initButton['font'] = buttonFont
+
+exitButton = Button(root, text='CLOSE', width=17, fg='red', command=ExitApplication)
+exitButton['font'] = buttonFont
+
+sysprimeButton = Button(root, text='PRIME', width=17, bg='paleturquoise', command=sysprime)
+sysprimeButton['font'] = buttonFont
+
+originButton = Button(root, text='OFFSET POS.', bg='darkorange', command=origin)
+originButton['font'] = buttonFont
+
+fillButton = Button(root, text='FILL', bg='deepskyblue', height= 3, command=fill)
+fillButton['font'] = buttonFont
+
+emptyButton = Button(root, text='EMPTY', bg='IndianRed1', command=empty)
+emptyButton['font'] = buttonFont
+
+babaluButton = Button(root, text='RUN\n BABALU', bg='purple1', height=4, command=babalu)
+babaluButton['font'] = buttonFont
+
+falconButton = Button(root, text='RUN\n Falcon', bg='purple1', height=4, command=falcon)
+falconButton['font'] = buttonFont
+
+cardeaButton = Button(root, text='RUN\n CARDEA', bg='orchid1', height=4, command=cardea) 
+cardeaButton['font'] = buttonFont
+
+gaiaButton = Button(root, text='RUN\n GAIA', bg='orchid1', height=4, command=gaia) 
+gaiaButton['font'] = buttonFont
+
+sysprimeButton.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
+#originButton.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
+initButton.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
+exitButton.grid(row=0, column=2, padx=5, pady=5, sticky='nsew')
+fillButton.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
+emptyButton.grid(row=1, column=2, padx=5, pady=5, sticky='nsew')
+babaluButton.grid(row=2, column=0, padx=5, pady=5, sticky='nsew')
+falconButton.grid(row=2, column=2, padx=5, pady=5, sticky='nsew')
+cardeaButton.grid(row=3, column=0, padx=5, pady=5, sticky='nsew')
+gaiaButton.grid(row=3, column=2, padx=5, pady=5, sticky='nsew')
+
+if __name__ == '__main__':
+    root.mainloop()
